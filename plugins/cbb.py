f19277ad1762079b8249854a46ea189c9628084b
Sanith S <83565729+Sanith643@users.noreply.github.com>
2022-07-18 19:59:44 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b> ○ᴄʀᴇᴀᴛᴏʀ : <a href='https://t.me/Rahulthrilok'>ᴊᴜɪᴄᴇ ᴡʀʟᴅ࿐</a>\n○ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://t.me/Rahulthrilok'>Click here</a>\n○ ᴜᴘᴅᴀᴛᴇꜱ : @Series_Linkz\n○ ᴍʏ ɢʀᴏᴜᴘ : @Seriesloverskerala</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
